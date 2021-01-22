4cc3621d7cc05e7210a0f5219107e2084c73a360
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-01-21 16:40:54 -0800

    added tests for io and logging

--- terra/tests/test_io.py
+++ terra/tests/test_io.py
@@ -1,8 +1,9 @@
 import os
 import numpy as np
+import pandas as pd
 
 from terra.utils import ensure_dir_exists
-from terra.io import Artifact
+from terra.io import Artifact, json_dump, rm_nested_artifacts, json_load
 from terra.settings import TERRA_CONFIG
 import terra.database as tdb
 
@@ -45,7 +46,35 @@ def test_artifact_rm(tmpdir):
     artifact = Artifact.dump(value=x, run_dir=run_dir)
     
     artifact.rm()
+
+    assert(not os.path.isfile(artifact._get_path()))
+    # check that artifact is reflected as removed in the daabase
+    df = tdb.get_artifact_dumps(run_ids=1)
+    assert(df.iloc[0].rm)
+
+
+def test_rm_nested_artifacts(tmpdir):
+    TERRA_CONFIG["storage_dir"] = str(tmpdir)
+    tdb.Session = (
+        tdb.get_session()
+    )  # need to recreate Session with new tmpdir
+
+    run_dir = os.path.join(tmpdir, str(1))
+    ensure_dir_exists(run_dir)
+    json_dump({
+        "a": [1, 3, np.random.rand(10)],
+        "b": pd.DataFrame({"a": [1,2,3], "b": [4,5,6]}),
+        "c": [1, 2, 3, {
+            "a": np.random.rand(10)
+        }]
+    }, run_dir=run_dir, path=os.path.join(run_dir, "out.json"))
     
+    out = json_load(os.path.join(run_dir, "out.json"))
+    rm_nested_artifacts(out)
+
+    # check that artifacts are reflected as removed in the daabase
+    df = tdb.get_artifact_dumps(run_ids=1)
+    assert(df.rm.all())
 
 
 
---
+++ terra/tests/test_logging.py
@@ -0,0 +1,33 @@
+import os
+
+from terra import Task
+from terra.logging import Logger, init_logging
+from terra.settings import TERRA_CONFIG
+import terra.database as tdb
+
+
+TERRA_CONFIG["notify"] = False
+
+
+def test_logging(tmpdir):
+    TERRA_CONFIG["storage_dir"] = str(tmpdir)
+    tdb.Session = (
+        tdb.get_session()
+    )  # need to recreate Session with new tmpdir
+
+    stdout_str = "qwertyuiop"
+    stderr_str = "asdfghjkl"
+
+    @Task.make_task
+    def fn_a(run_dir=None):
+        print(stdout_str)
+        raise ValueError(stderr_str)
+    try:
+        fn_a()
+    except ValueError:
+        pass 
+    log = fn_a.get_log()
+
+    assert(stdout_str in log)
+    assert(stderr_str in log)
+
