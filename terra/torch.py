7b05583768af2842281f6c4bbb4df1d5d3651629
Sabri Eyuboglu <eyuboglu@sh01-ln02.stanford.edu>
2021-02-22 11:01:56 -0800

    Added TerraModule to terra.torch.

--- terra/torch.py
+++ terra/torch.py
@@ -10,3 +10,16 @@ def write_tensor(out, path):
 @reader(torch.Tensor)
 def read_tensor(path):
     return torch.load(path)
+
+
+class TerraModule:
+
+    def __terra_write__(self, path):
+        torch.save({"state_dict": self.state_dict(), "config": self.config}, path)
+
+    @classmethod
+    def __terra_read__(cls, path):
+        dct = torch.load(path)
+        model = cls(dct["config"])
+        model.load_state_dict(dct["state_dict"])
+        return model
