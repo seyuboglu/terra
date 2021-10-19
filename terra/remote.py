9f43f0a66956d5e42ae25db5430ef0090beef16c
Sabri Eyuboglu <eyuboglu@stanford.edu>
2021-10-19 00:16:31 +0000

    Address issue with in progress runs

--- terra/remote.py
+++ terra/remote.py
@@ -82,6 +82,7 @@ def push(
     for run in runs:
         if run.status == "in_progress":
             warn(f"Skipping run_id={run.id} because the run's in progress.")
+            continue
         rel_path = to_rel_path(run.run_dir)
         abs_path = to_abs_path(run.run_dir)
 
