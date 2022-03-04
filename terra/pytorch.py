2e0e808d9ea4d6270fc4d7e55f06fe6c05c9b1b6
Sabri Eyuboglu <eyuboglu@stanford.edu>
2022-03-04 03:31:18 +0000

    Add callback

--- terra/pytorch.py
+++ terra/pytorch.py
@@ -1,3 +1,6 @@
+import os
+from typing import Any, Dict
+
 import torch
 
 from terra.io import reader, writer
@@ -22,6 +25,33 @@ class TerraModule:
     def __terra_read__(cls, path):
         # TODO: make it possible to specify a gpu
         dct = torch.load(path, map_location="cpu")
-        model = cls(dct["config"])
+        model = cls(config=dct["config"])
         model.load_state_dict(dct["state_dict"])
         return model
+
+    def on_save_checkpoint(self, checkpoint: Dict[str, Any]) -> None:
+        dirpath = None
+        for k, v in checkpoint["callbacks"].items():
+            if isinstance(k, str) and k.startswith("ModelCheckpoint"):
+                dirpath = v["dirpath"]
+                best_model_path = v["best_model_path"]
+                break
+
+        if dirpath is None:
+            return
+
+        if os.path.exists(best_model_path):
+            # this is not the best model, so do not dump
+            return
+
+        from terra import Task
+
+        Task.dump(
+            {
+                "model": self,
+                "valid": self.valid_preds.compute(),
+            },
+            run_dir=dirpath,
+            group_name="best_chkpt",
+            overwrite=True,
+        )
